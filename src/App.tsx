76116462627e12cb13e32956c02e89b28b52d63f
Rodrigo Monteiro <rodrigomonteiiro17@gmail.com>
2023-03-20 15:21:58 -0300

    Create project and add Interfaces

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react'
+function App() {
+  let message: string = "Title Global"
+  return (
+    <div className="App">
+      {message}
+    </div>
+  );
+}
+
+export default App;
